ca15c61f156ff388569b03ee298109249d556a81
Jordan J <jordanr2m@gmail.com>
2023-10-15 14:53:58 -0700

    Working on updates.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,12 @@
-
-let accordianHeadings = document.querySelectorAll(".accordian-heading");
-console.log(accordianHeadings);
-
-const accordianContent = document.querySelectorAll(".accordian-content");
-console.log(accordianContent);
+const accordianItems = document.querySelectorAll(".accordian-item");
+// console.log(accordianItems);
 
 
-accordianHeadings.forEach(function (i) {
-    i.addEventListener("click", function () {
-        accordianContent.classList.toggle("hidden");
-    })
-})
-
-// accordianHeading.forEach(function (item) {
-//     item.addEventListener("click", function () {
-//         const isItemOpen = item.classList.contains("hidden"); // check if item is opened first
-//         accordianHeading.forEach(function (item) {
-//             item.classList.remove("hidden");
-//         })
-//         if (!isItemOpen) {
-//             item.classList.toggle("hidden");
-//         }
-//     })
-// })
+// Code to make a simple accordian
+for (i = 0; i < accordianItems.length; i++) {
+    accordianItems[i].addEventListener("click", function () {
+        // Toggle between adding and removing the "active" class,
+        // to highlight the button that controls the panel 
+        this.classList.toggle("active");
+    });
+}
